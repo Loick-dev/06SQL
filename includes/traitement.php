8c31989c79b3064194ef2d28fb88635f298174b4
Loick-dev <loick.carballido@gmail.com>
2021-01-14 16:37:06 +0100

    maj1.0

---
+++ includes/traitement.php
@@ -0,0 +1,11 @@
+<?php
+    require_once('bdd.inc.php');
+
+    $req = $bdd->prepare('INSERT INTO apprenants (prenom, codepostal, ville) VALUES (?, ?, ?)');
+    $req->execute(array($_POST['prenom'], $_POST['codepostal'], $_POST['ville']));
+    
+    echo '<prep>';
+    var_dump($bdd);
+    var_dump($req);
+    echo '</prep>';
+?>
