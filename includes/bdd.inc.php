81034b7a44f834d1e5bd75d7532c5a6203b7477d
Loick-dev <loick.carballido@gmail.com>
2021-01-14 12:31:44 +0100

    connexion bdd

---
+++ includes/bdd.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+    $host = 'localhost';
+    $db = '06-sql';
+    $user = 'root';
+    $password ='';
+
+    $bdd = new PDO('mysql:host=' . $host . ';dbname=' . $db . '; charset=utf8', $user, $password);
+  //  $bdd = new PDO('mysql:host=localhost:dbname=06-sql;chaset=utf8', 'root', 'root' );
+
+?>
