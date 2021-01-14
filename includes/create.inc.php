ad8d2477e9f7d8fca39f0db7e0e916eccc893ded
Loick-dev <loick.carballido@gmail.com>
2021-01-14 16:47:17 +0100

    last maj

--- includes/create.inc.php
+++ includes/create.inc.php
@@ -1,4 +1,4 @@
-<h2>Ajouter</h2>
+<!-- <h2>Ajouter</h2>
 <form action="./includes/traitement.php" method="post">
     <div class="form-groupe">
         <label for="prenom">Prénom</label>
@@ -19,4 +19,24 @@
         <button type="submit" class="btn btn-dark">Envoyer</button>
     </div>
 
+</form> -->
+
+<h2>Ajouter</h2>
+
+<form action="./includes/traitement.php" method="post">
+    <div class="form-group">
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" placeholder="Ex.: Jean-Pierre" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="codepostal">Code postal</label>
+        <input type="number" name="codepostal" placeholder="12345" class="form-control">
+    </div>
+    <div class="form-group">
+        <label for="ville">Ville</label>
+        <input type="text" name="ville" placeholder="Ex.: Paris" class="form-control">
+    </div>
+    <div class="text-right">
+        <button type="submit" class="btn btn-dark">Envoyer</button>
+    </div>
 </form>
