357f6149879573583b9230a2ace8d26559aafa59
Loick-dev <loick.carballido@gmail.com>
2021-01-14 15:50:39 +0100

    mise à jour

---
+++ includes/create.inc.php
@@ -0,0 +1,22 @@
+<h2>Ajouter</h2>
+<form action="./includes/traitement.php" method="post">
+    <div class="form-groupe">
+        <label for="prenom">Prénom</label>
+        <input type="text" name="prenom" placeholder="Ex.: Jean-Pierre" class="form-control">
+
+    </div>
+    <div class="form-groupe">
+        <label for="codepostal">Code Postal</label>
+        <input type="number" name="codepostal" placeholder="Ex.: 75006" class="form-control">
+
+    </div>
+    <div class="form-groupe">
+        <label for="ville">Ville</label>
+        <input type="text" name="ville" placeholder="Ex.: Paris" class="form-control">
+
+    </div>
+    <div class="text-right">
+        <button type="submit" class="btn btn-dark">Envoyer</button>
+    </div>
+
+</form>
--- index.php
+++ index.php
@@ -7,24 +7,43 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<?php
-    require('./templates/head.inc.php');
-?>
+    <?php
+        require('./templates/head.inc.html');
+    ?>
 </head>
 <body>
     <header>
-    <?php
-    require('./templates/header.html');
-?>
+        <?php
+            require('./templates/header.html');
+        ?>
     </header>
-    <main>
-    <?php
-    if
-    ?></main>
+    <main class="container-fluid">
+    <div class="row">
+        <div class="col-3">
+            <?php
+                require('./templates/nav.html');
+            ?>
+        </div>
+        <div class="col-9">
+        <?php   
+            if(isset($_GET['read'])){
+                include('./includes/read.inc.php');
+            }
+            elseif(isset($_GET['create'])){
+                include('./includes/create.inc.php');
+            }
+            else{
+                echo '<h2 class=p-5>¡Bienvenido!</h2>';
+            }
+
+        ?>
+        </div>
+    </div>
+    </main>
     <footer>
-    <?php
-    require('./templates/footer.html');
-?>
+        <?php
+            require('./templates/footer.html');
+        ?>
     </footer>
 
 </body>
