191b1e9487fe8fde66dad10c84ad30097a0322d3
Loick-dev <loick.carballido@gmail.com>
2021-01-14 12:53:01 +0100

    structure template

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
+<?php
+
+    require_once('includes/bdd.inc.php');
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php
+    require('./templates/head.inc.php');
+?>
 </head>
 <body>
-  <h1>Hello world!</h1>  
+    
 </body>
 </html>
